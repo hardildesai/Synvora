ac0d1e14553a7bfd21e94577623b9697f146606f
Hardil Desai <hardil.bd@gmail.com>
2025-09-02 05:20:39 +0000

    30% more earlier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,11 +21,11 @@ const Header = () => {
     if (heroCountdownEl) {
       const observer = new IntersectionObserver(
         ([entry]) => {
-          const isVisible = entry.intersectionRatio > 0.68;
+          const isVisible = entry.intersectionRatio > 0.98;
           setIsHeroCountdownVisible(isVisible);
           setIsHeroCtaVisible(isVisible); // Tie the CTA visibility to the same observer
         },
-        { threshold: 0.68 } 
+        { threshold: 0.98 } 
       );
       observer.observe(heroCountdownEl);
        return () => observer.disconnect();
