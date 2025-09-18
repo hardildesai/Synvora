0f87d4df482032f897d43139c63eff0feda2572b
Hardil Desai <hardil.bd@gmail.com>
2025-09-18 18:28:41 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your project's Firebase configuration
 const firebaseConfig = {
-  "projectId": "rhythmpass",
-  "appId": "1:838786524087:web:31ccdce567d221e82afe32",
-  "storageBucket": "rhythmpass.firebasestorage.app",
-  "apiKey": "AIzaSyBHCbLT7vzyXu3jJdSVY8JzdvChDJeEZS8",
-  "authDomain": "rhythmpass.firebaseapp.com",
-  "messagingSenderId": "838786524087"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase for SSR
